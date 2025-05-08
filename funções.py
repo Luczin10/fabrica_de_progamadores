db95ac4771280c6b32fb2f24c52dfc028025c3b5
Luczin10 <lucasscomparin17@gmail.com>
2025-05-08 15:51:05 -0300

    Add files via upload

---
+++ funções.py
@@ -0,0 +1,32 @@
+texto1="este livro é legal"
+texto2="vou aprender a progamar"
+texto1=texto2
+#print(texto1)
+
+Nome= "Lucas"
+Sobrenome=" Gabriel"
+nome_completo= Nome + Sobrenome
+#print("Olá") 
+
+ganho=150
+roubo=3
+balas=20
+balas-=roubo
+#print(balas)
+balas+=ganho
+#print(balas)
+
+#nome=input()
+#Saudação="Olá "+ nome
+#print(Saudação)
+numero1=input()
+numero_1=int(numero1)
+#resultado=numero_1-2
+numero2=input()
+numero_2=int(numero2)
+resultado=numero_1 * numero_2
+print(resultado)
+
+
+
+
