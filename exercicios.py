de9f1df5fec319f11f540ed761e6c17a73e43b4a
Luczin10 <lucasscomparin17@gmail.com>
2025-05-09 15:33:42 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,33 @@
+#ex001
+LAqua=int(input()) #cm
+ARqua=LAqua*LAqua
+resultado1=ARqua
+print("Resultado",resultado1)
+
+
+#ex002
+Salario=float(input())
+ajuste=Salario*0.15
+newsalario=Salario+ajuste
+print("Ajuste é de ",newsalario)
+
+
+#ex003
+BASEtri=int(input())
+HIGHtri=int(input())
+ARtri=BASEtri*HIGHtri/2
+print("Sua area é de ",ARtri)
+
+#ex004
+num1=int(input())
+num2=int(input())
+print("Antes",num1,num2)
+num3=""
+num3=num1
+num1=""
+num1=num2
+num2=num3
+print("depois",num1,num2)
+
+
+
