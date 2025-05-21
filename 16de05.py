34018657ad44ba8267db49395be9ef07ad258b77
Luczin10 <lucasscomparin17@gmail.com>
2025-05-21 14:08:23 -0300

    Add files via upload

---
+++ 16de05.py
@@ -0,0 +1,35 @@
+a=1
+b=0
+c=1
+if (a and a):
+    print("true")
+else:
+    print("false")
+if (b and b):
+    print("true")
+else:
+    print("false")
+if (not c):
+    print("true")
+else:
+     print("false")
+if (not b):
+    print("true")
+else:
+     print("false")
+if (not a):
+    print("true")
+else:
+     print("false")
+if (a and b):
+    print("true")
+else:
+     print("false")
+if (b and c):
+    print("true")
+else:
+     print("false")
+if (a or c):
+    print("true")
+else:
+     print("false")
