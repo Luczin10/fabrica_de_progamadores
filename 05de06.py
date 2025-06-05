339541d91e953325522910c5764a7fab21dd0c7c
Luczin10 <lucasscomparin17@gmail.com>
2025-06-05 15:29:52 -0300

    Add files via upload

---
+++ 05de06.py
@@ -0,0 +1,10 @@
+import random
+while True:
+    tigrinho= random.randint(0,10)
+    if tigrinho<9:
+        print("\033[31m perdeuuuuuuuuuuu\033[0m")
+    
+    else:
+        print("ganhou, aff")
+
+    
