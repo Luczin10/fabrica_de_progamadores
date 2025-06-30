a5a257f56ae58f9ac4f570d33ad16f1e136d7c4b
Luczin10 <lucasscomparin17@gmail.com>
2025-06-30 15:48:54 -0300

    Add files via upload

---
+++ 30de06.py
@@ -0,0 +1,26 @@
+import os
+import random
+#print(os.getcwd())
+'''
+try:
+    os.mkdir("d")
+    os.mkdir("e")
+    os.mkdir("f")
+
+
+    
+    os.rmdir("d")
+    os.rmdir("e")
+    os.rmdir("f")
+    
+
+except:
+    print("erro")
+'''
+
+os.remove("arquivo.txt")
+
+ 
+   
+
+
